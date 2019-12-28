e051e08068742f5eaa03bf31bf334b48475e199b
SergiooOliveira <58986379+SergiooOliveira@users.noreply.github.com>
2019-12-28 19:41:34 +0000

    Create Arrays.h

---
+++ .github/workflows/Project/Arrays.h
@@ -0,0 +1,22 @@
+#pragma once
+char teamsNames[18][30] =
+{
+	"Belenenses SAD",
+	"Boavista",
+	"CD Santa Clara",
+	"CD Tondela",
+	"Clube Desportivo das Aves",
+	"Famalicão",
+	"FC Paços de Ferreira",
+	"FC Porto",
+	"Gil Vicente",
+	"Maritimo",
+	"Moreirense FC",
+	"Portimonense SC",
+	"Rio Ave FC",
+	"SC Braga",
+	"SL Benfica",
+	"Sporting CP",
+	"Vitória FC",
+	"Vitória SC"
+};
