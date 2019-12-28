7c48b09da5e4b5af7df8eca29c1052c642badbda
SergiooOliveira <58986379+SergiooOliveira@users.noreply.github.com>
2019-12-28 19:40:57 +0000

    Create FuncsHeader.h

---
+++ .github/workflows/Project/FuncsHeader.h
@@ -0,0 +1,20 @@
+#pragma once
+long CalcTransferValue(int salary, int monthsLeft);
+
+long NewSalary(int salary);
+
+float RandInRange(float min, float max);
+
+float TicketsProfit(int maxCapacity);
+
+void addCalendar(char calendar[306][60]);
+
+void CreateCalendar(char teams[18][30]);
+
+void ShowTeamsList(char teams[18][30]);
+
+void StartNewSeason();
+
+void NewJourney();
+
+void readTeamInfo(char teamNames[18][60]);
